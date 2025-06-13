3c6ea8bdf0a9e4700f054255dd7b6b9ef2d49fa5
Veron0013 <iv_mirsoft@ukr.net>
2025-06-13 15:52:32 +0000

    Deploying to gh-pages from @ Veron0013/goit-js-hw-11@18fa4aa5504ba2641caa3a08d2f7396922959044 🚀

--- index.js
+++ index.js
@@ -1,2 +1,27 @@
-import{a as l,i as f}from"./assets/vendor-BaWOTRH5.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))a(e);new MutationObserver(e=>{for(const o of e)if(o.type==="childList")for(const i of o.addedNodes)i.tagName==="LINK"&&i.rel==="modulepreload"&&a(i)}).observe(document,{childList:!0,subtree:!0});function t(e){const o={};return e.integrity&&(o.integrity=e.integrity),e.referrerPolicy&&(o.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?o.credentials="include":e.crossOrigin==="anonymous"?o.credentials="omit":o.credentials="same-origin",o}function a(e){if(e.ep)return;e.ep=!0;const o=t(e);fetch(e.href,o)}})();const d="50823959-4aaa76fee656d12ff3aaa5105",c="https://pixabay.com/api/",u="./img/error.svg",n={key:d,q:"",image_type:"photo",orientation:"horizontal",safesearch:"true",per_page:"9"};function g(s){console.log(c,s);const r=new URLSearchParams(s).toString();l(`${c}?${r}`).then(t=>{console.log(t.data.hits),t.data.hits.length>0?console.log(t.hits):f.show({id:"error",title:"Error",message:"Sorry, there are no images matching your search query. Please, try again!",messageColor:"white",color:"#ef4040",position:"topCenter",iconUrl:u})}).catch(t=>{console.log(t)}).finally(()=>{loader.classList.add("hidden")})}console.log(n);n.q="cars";g(n);
+import{a as f,S as h,i as p}from"./assets/vendor-Bg_GrDtl.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))l(e);new MutationObserver(e=>{for(const s of e)if(s.type==="childList")for(const a of s.addedNodes)a.tagName==="LINK"&&a.rel==="modulepreload"&&l(a)}).observe(document,{childList:!0,subtree:!0});function i(e){const s={};return e.integrity&&(s.integrity=e.integrity),e.referrerPolicy&&(s.referrerPolicy=e.referrerPolicy),e.crossOrigin==="use-credentials"?s.credentials="include":e.crossOrigin==="anonymous"?s.credentials="omit":s.credentials="same-origin",s}function l(e){if(e.ep)return;e.ep=!0;const s=i(e);fetch(e.href,s)}})();const m="50823959-4aaa76fee656d12ff3aaa5105",y="https://pixabay.com/api/",n={key:m,q:"",image_type:"photo",orientation:"horizontal",safesearch:"true",per_page:"99"};function g(r){const t=new URLSearchParams(r).toString();return f(`${y}?${t}`).then(i=>i.data.hits).catch(i=>[])}const _=document.querySelector(".gallery"),d=document.querySelector(".loader_cont");let L=new h(".gallery a",{captions:!0,captionsData:"alt",captionPosition:"bottom",animationSpeed:250,showCounter:!1});function S(r){_.innerHTML=R(r),L.refresh()}function c(){_.innerHTML=""}function w(){d.classList.remove("hidden")}function u(){d.classList.add("hidden")}function R(r){return r.map(t=>t.previewURL&&t.largeImageURL&&t.tags?`<li class="data_list_items">
+				<a class="gallery_link" href="${t.largeImageURL}">
+					<img
+						class="_list_img"
+						src="${t.previewURL}" 
+						alt="${t.tags}"/>		
+					<ul class="_list_attr">
+						<li class="_list_attr_itm">
+							<h3 class="_list_attr_title">Likes</h3>
+							<p class="_list_attr_text">${t.likes}</p>
+						</li>
+						<li class="_list_attr_itm">
+							<h3 class="_list_attr_title">Views</h3>
+							<p class="_list_attr_text">${t.views}</p>
+						</li>
+						<li class="_list_attr_itm">
+							<h3 class="_list_attr_title">Comments</h3>
+							<p class="_list_attr_text">${t.comments}</p>
+						</li>
+						<li class="_list_attr_itm">
+							<h3 class="_list_attr_title">Downloads</h3>
+							<p class="_list_attr_text">${t.downloads}</p>
+						</li>
+					</ul>
+				</a>
+			</li>`:null).filter(t=>t!==null).join("")}const q="./img/error.svg",P="Sorry, there are no images matching your search query. Please, try again!",v="Sorry, there is error in your request. Please, try again later!",O="Sorry, there is error in your request. Please, try to write more than 3 letters!",$="Sorry, there is error in your request. Please, try to write not only digits!",G=document.querySelector(".form"),b=document.querySelector(".input_delay");function o(r){p.show({id:"error",title:"Error",message:r,messageColor:"white",color:"#ef4040",position:"topCenter",iconUrl:q})}function x(r){return r.length<3?(o(O),!1):/^\d+$/.test(r)?(o($),!1):!0}G.addEventListener("submit",E);function E(r){r.preventDefault();const t=b.value.trim();x(t)&&(w(),n.q=t,g(n).then(i=>{if(i.length===0){c(),o(P);return}S(i),u()}).catch(i=>{o(v),c(),u()}))}
 //# sourceMappingURL=index.js.map
